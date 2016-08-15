bcef91a789bde4b35e358a81390a30cb55122a2a
goliatone <burgosemi@gmail.com>
2016-08-15 11:09:48 -0400

    adding main entrypoint

---
+++ index.js
@@ -0,0 +1,14 @@
+/*jshint esversion:6, node:true*/
+/*
+ * Influx Line Protocol Parser
+ * https://github.com/goliatone/influx-line-protocol-parser
+ *
+ * Copyright (c) 2016 goliatone
+ * Licensed under the MIT license.
+ */
+'use strict';
+
+/**
+ * Exports main library
+ */
+module.exports = require('./lib');
